d9f3135e196b5dd40702ea02f34b3772e6ecce4d
z4phkiel <vincentanthonydelaserna@gmail.com>
2025-09-01 12:03:07 +0800

    code updated 01/09/2025

--- api/getplayers.php
+++ api/getplayers.php
@@ -4,16 +4,32 @@ require_once __DIR__ .'/../main/db/connection.php';
 $db = new Database();
 $conn = $db->connect();
 
-
+header('Content-type: application/json');
 try {
 
+    if(isset($_GET['id'])) {
+        $id = intval($_GET['id']);
+        $stmt = $conn->prepare("SELECT * FROM playerstats WHERE player_id = :id");
+        $stmt->execute(["id" => $id]);
+        $player = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $stmt = $conn->query('SELECT * FROM playerstats');
-$playerlist = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($player) {
+            echo json_encode($player);
+        } else {
+            echo json_encode(['error' => 'No player found!']);
+        }
+        
 
-header('Content-type: application/json');
+    } else {
 
-echo json_encode($playerlist);
+        $stmt = $conn->prepare("SELECT * FROM playerstats");
+        $stmt->execute();
+        $players = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        echo json_encode($players);
+    }
+
+
 
 } catch (PDOException $e) {
 echo json_encode(["error" => $e->getMessage()]);
